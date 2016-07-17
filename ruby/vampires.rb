d32ce145a92599f3b28f253794b43da9ab4dcc14
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-16 23:31:27 -0400

    complete release 1 ask for input

---
+++ ruby/vampires.rb
@@ -0,0 +1,18 @@
+puts "what is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age =  gets.chomp
+
+puts " what year were you born?"
+birth_year = gets.chomp
+
+puts "Would you like and order of  garlic bread?"
+gar_bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance plan?"
+insurance = gets.chomp
+
+
+
+
