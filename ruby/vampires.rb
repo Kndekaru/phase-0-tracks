a7ceabc3a2dbd656e410cf6face75634efcb2d5e
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-16 23:45:37 -0400

    complete release2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,6 @@
+# Miles McArdle-coe
+#Gets input for vampire screening
+
 puts "what is your name?"
 name = gets.chomp
 
@@ -13,6 +16,24 @@ gar_bread = gets.chomp
 puts "Would you like to enroll in the company’s health insurance plan?"
 insurance = gets.chomp
 
+#age correction to search for vampire
+age_correct = (DateTime.year - birth_year) == age
 
+	if age && (gar_bread || insurance)
+		puts "probabaly not a vampire"
+	elsif !age_correct && (garlic || insurance)
+		puts "probabaly a vampire"
+	elsif !age_correct && !gar_bread && insurance
+		puts "Almost certainly a vampire"
+	elsif name
+		puts "definetly a vampire"
+	elsif 
+		puts "Inconclusive results"
+	end
+		
+		
+
+
+		
 
 
