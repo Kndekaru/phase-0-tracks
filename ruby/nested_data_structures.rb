f3e59100f2f046c4514f204c0162e3d4a169cb78
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-21 20:28:21 -0400

    add plumbing,tool,paint and hardware departments

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,4 +1,67 @@
-Restaurants = {
-	Longhorn_Steakhouse: {
-		Short_name: "Longhorn"
-		} "
+Hardware_store = {
+	Tool_dept: {
+		Capacity: {
+			Aisles: 4,
+			shelves: 100
+		}
+		Items:[
+			"Hammers",
+			"screwdrivers",
+			"pliers",
+			"wrenches",
+			"vicegrips",
+			"files",
+			"level",
+			"tape measure"
+		]
+	}
+	Plumbing_dept:{
+		Capacity: {
+			Aisles: 8
+			shelves: 200
+		}
+		Items:[
+			"PVC pipe",
+			"Pipe Glue",
+			"Plumbers wrench",
+			"loose fittings",
+			"cable saw",
+			"toilet seals",
+			"Plumbers tape"
+		]	
+	}
+	Paint_dept:{
+		Capacity:{
+			Aisles:3,
+			shelves:75
+		}
+		Items: [
+			"Color swatches",
+			"Terry Cloth",
+			"Tarps",
+			"Paint brushes",
+			"Paint rollers",
+			"Empty paint cans",
+			"Tint"
+			"Gallon paint bases"
+			"Quart paint bases"
+		]
+	}
+	Hardware_dept:{
+		Capacity: {
+			Aisles: 4,
+			shelves: 75,
+			bins: 200 
+		}
+		Items: [
+			"Screws",
+			"Nuts",
+			"Bolts",
+			"Anchors",
+			"Hinges",
+			"Assorted Lumber",
+			"Hooks",
+			"Specialty hardware"
+		]
+	}
+}
