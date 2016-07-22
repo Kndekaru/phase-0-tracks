120a864809f5849e04d1260f85e6ba3fc8f636c0
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-22 12:45:54 -0400

    add fibonacci calc

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,15 +1,27 @@
-def search_array(num1)
-  arr = [1, 2, 3, 4, 5]
-   for i in arr
-    if  User_number == arr[i] 
-      return "This number is in the array."
-    elsif User_number != arr[i]
-      return nil
-     end
-  end
+#def search_array(num1)
+#  arr = [1, 2, 3, 4, 5]
+#   for i in arr
+#    if  User_number == arr[i] 
+#      return "This number is in the array."
+#    elsif User_number != arr[i]
+#      return nil
+#     end
+#  end
+#end
+
+#p "Give me a number to search to for."
+#ser_number = gets.chomp.to_i
+
+#p search_array(User_number)
+
+def Fibonacci(n)
+  a = 0
+  b = 1
+  
+n.times do
+  temp = a
+  a = b
+  b = temp + b
 end
-
-p "Give me a number to search to for."
-User_number = gets.chomp.to_i
-
-p search_array(User_number)
+return a 
+end
