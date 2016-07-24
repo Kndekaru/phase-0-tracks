9366d5bed36d4fe2258234d9e15d4ad85b3f6e08
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-24 15:35:40 -0400

    add update interface and add comments

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,8 +7,10 @@
 #ask client to update old hash info 
 #return edited info
 #check if info need update for final submititon to sata structure
-Client_info = {}
 
+#initlialize hash
+Client_info = {}
+#prompt user for info
 Puts "enter your name please"
 Client_info[name] = gets.chomp
 puts "enter your age"
@@ -19,3 +21,19 @@ puts "enter your decor theme"
 Client_info[decor] = gets.chomp
 puts "enter your prefered retailer"
 Client_info[retail] = gets.chomp
+#return info to user
+p Client_info
+#ask to confirm informatipn
+puts "Is the information correct? If information is correct type Correct if information is incorrect type Incorrect"
+confirmation = gets.chomp
+	#check input
+	if confirmation = Correct
+		puts "congratulations your information has been added succesfully"
+	else 
+		puts "Please type incorrect key value"
+		change_key = gets.chomp
+		puts "Please ente correct information"
+		new_info = gets.chomp
+		Client_info[change_key.to_sym] = new_info
+	end
+p Client_info 
