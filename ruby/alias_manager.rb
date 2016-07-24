0739b984f61d308fc2b14b7695b0175d6b203395
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-24 18:18:04 -0400

    initial commit,pseudo code

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,10 @@
+#take input and have names stored as variables
+#Use split to create arrays
+#iterate throught the array  and check charcters equvivlancy to the vowel value via a method
+#have method convert vowels to next value
+#join these new letters toegtehr 
+#repeat this process for consonants and store  bow in new variables
+
+#modify user interface to take quit as a valid option
+# create an empty hash for name storage 
+#store old and modified names into hash for later access
