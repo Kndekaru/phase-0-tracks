29d787c4ec4b9c16a48716107f2a81eb301920ed
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-24 15:26:52 -0400

    prompt user for input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,3 +7,15 @@
 #ask client to update old hash info 
 #return edited info
 #check if info need update for final submititon to sata structure
+Client_info = {}
+
+Puts "enter your name please"
+Client_info[name] = gets.chomp
+puts "enter your age"
+Client_info[age] = gets.chomp.to_i
+puts "enter your number of children"
+Client_info[kids] = gets.chomp.to_i
+puts "enter your decor theme"
+Client_info[decor] = gets.chomp
+puts "enter your prefered retailer"
+Client_info[retail] = gets.chomp
