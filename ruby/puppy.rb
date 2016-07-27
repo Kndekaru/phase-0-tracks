63719868e54d704f48513a9986bef729610d6e31
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-27 13:37:49 -0400

    initial commit, Made psuedo class declaration

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+SPECIES ------------------------
+Canis lupus familiaris
+
+CHARACTERISTICS ----------------
+Size: Large
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: Brown,white,or black
+Atheletic: Yes
+
+BEHAVIOR -----------------------
+Jump
+Bark
+Fetch
+Play
+Beg
+Roll over
