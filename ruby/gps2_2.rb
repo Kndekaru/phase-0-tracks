57fca1cd28c06249fb0c0160f2a49cd8dc454b57
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-27 16:18:04 -0400

    release 3

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,28 +1,94 @@
-#Miles McArdle-coe gps 2.2 with 
+#Miles McArdle-coe gps 2.2 with Kimberly sauzo
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps: 
-  # [fill in any steps here]
-  # set default quantity
-  # print the list to the console [can you use one of your other methods here?]
-# output: [what data type goes here, array or hash?]
+  # define the name of our method
+  # use .split to seperate the string used for input
+  # set default quantity we will use 1
+  # print the list to the console [use list print method]
+# output: our output will be a hash of the list
 
 # Method to add an item to a list
-# input: item name and optional quantity
-# steps:
-# output:
+# input: have user input/ get item name and optional quantity 
+# steps: add to hash list 
+# output:updated hash
 
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+# input: will be name of key value user wishes to remove 
+# steps:remove #{key vale} from hash we will use hash.delete method
+# output:updated hash 
 
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+# input: item name and seperatly ask for quantitiy to update
+# steps:use item as a key and quantity as a value to be updated
+# output:updated hash that will have both key and value updated
 
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+# input: The hash previously created
+# steps: use .each to print each key and corresponding value
+# output: a printed list
+
+def list_print(shopping_list)
+  puts "----------Shopping List----------"
+  shopping_list.each do |x,y|
+    puts "#{x}, #{y}" 
+  end
+end
+def create_list(string)
+  item_array = string.split(" ")
+  shopping_list = {}
+  item_array.each do |item|
+    shopping_list[item] = 1
+  end
+  #look to be refactored
+  shopping_list
+end
+
+def add_item(list_item,user_list)
+  # user_list = {}
+  user_list[list_item] = 1
+  
+  user_list
+end
+
+def remove_item(list_item,user_list)
+  user_list.delete(list_item)
+  user_list
+end
+ def update_quantity(list_item,quantity,user_list)
+   user_list[list_item] =  quantity
+   user_list
+ end
+
+# driver code
+list = "lemonade tomatoes onions ice-cream"
+user_list = create_list(list)
+puts user_list
+
+add_item('apple', user_list)
+p user_list 
+remove_item('apple', user_list)
+p user_list 
+update_quantity('lemonade', 2, user_list)
+p user_list
+list_print(user_list)
+
+#release 3
+update_quantity('tomatoes', 3, user_list)
+update_quantity('ice-cream', 4, user_list)
+remove_item('lemonade', user_list)
+list_print(user_list)
+update_quantity('ice-cream', 1, user_list)
+list_print(user_list)
+
+# add_item("apple")
+
+
+# create your list
+# add an item to your list
+# update an item quanitty
+
+
+
+#User Interface 
+#puts "Please enter items you would like to add to your shopping list followed by ENTER after each item. When finished type \"done\"."
