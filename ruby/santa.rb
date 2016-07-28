4ba8f5acb415b26aee3c492719edce9fa0a81dbf
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-27 20:55:08 -0400

    release 2 undefined method issues

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -6,11 +6,28 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!" 
 	end
-	def initialize
+	def initialize(gender,ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity =enthinicty
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = rand(0..1672)
 	end
 end
 
 Claus = Santa.new
 p Claus.speak
 p Claus.eat_milk_and_cookies("chocolate chip")
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Attack-helicopter", "prefer not to say", "Mystical Creature (unicorn)", "Congolese" "N/A"]
+x == 0
+until x == 50
+	santas =Santa.new(example_genders,example_ethnicities)
+	p "Attributes of santa #{x}"
+	p "Age:#{santa.age}"
+	p "gender:#{santa.gender}"
+	P "enthinicty:#{santa.ethnicity}"
+	p "-----------------------------------------"
+	x += 1
+end
