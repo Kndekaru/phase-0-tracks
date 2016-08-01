35691bfb77378b5ca90b91000802aad100f11909
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-07-31 21:26:20 -0400

    tweak rspec test

--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -1,4 +1,16 @@
 require_relative "hangman"
-describe place_holder do |variable|
+describe redact do
+let (:Hangman) {Hangman.new}
+star with "converts a word to empty spaces" do
+	expect(hangman.redact("hello")).to eq (" _ _ _ _ _ ")
+end
+start with "converts a word array of letters" do
+	expect(hangman.check_game("hello")).to eq (" h,e,l,l,o ")
+end
+it "checks an array" do
+	expect(hangman.def([hello]).to eq ("")
+end
+
+
 	
 end
