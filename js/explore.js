5d95a04de7ed677b152b955c2220d5b51851b44b
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-02 17:29:18 -0400

    finish pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,14 @@
-#Miles McArdle-Coe
+/*Miles McArdle-Coe
+Declare variables
+ VAR str = "Hello"
+ var index_values
+ use for loop to iterate through the characters of variables
+check index values and store index values in a new variable 
+
+declare function 
+ for var set equal to  length of string - 1, tset x =  0;y >=0 ; x --,y +)
+ 	set var of index values to rn i and j
+ 	return index values
+
+ call console.log and funtion on Hello
+*/
