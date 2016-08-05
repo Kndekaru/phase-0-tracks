0d0b08a76936869646727a4986a68e5003cdbb2b
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-04 20:26:36 -0400

    add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+
+*/ Pseudocode release 0
+delcare function longest_word and have it take an array 
+create empty array to store longest
+	iterate through the array using for var i = 0 
+		if input array length >word.length
+			set equal to on another
+		else 
+			longest item = longest word
+		print return value
+
+
+/*
