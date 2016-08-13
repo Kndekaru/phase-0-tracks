5e403986bc993d4d261ea94350eec4a1f5c14db8
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-13 19:22:35 -0400

    initial commit/craete name swap

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,51 +1,14 @@
-#take input and have names stored as variables
-#Use split to create arrays
-#iterate throught the array  and check charcters equvivlancy to the vowel value via a method
-#have method convert vowels to next value
-#join these new letters toegtehr 
-#repeat this process for consonants and store  bow in new variables
 
-#modify user interface to take quit as a valid option
-# create an empty hash for name storage 
-#store old and modified names into hash for later access
 
-alias_hash = {}
-def name_return(alias_hash)
-  alias_hash.each do |key, value|
-    puts "#{key} is also known as #{value}."
-   end
+def name_swapper(user_name)
+  swapped_name = user_name.split(' ').reverse!
+  print swapped_name
 end
-def modify_hash(alias_hash, name, new_name)
-  alias_hash[name] = new_name
-end
-def create_alias(name_split)
+def voel chang
+
+puts  "Please enter your full name!"
+user_name = gets.chomp 
+name_swapper(user_name) 
+
   vowels = "aeiou"
   consonants = "bcdfghjklmnpqrstvwxy"
-  item_split.map! do |string|
-    if vowels.index(string) != nil
-      new_vowel = vowels.index(string) + 1
-        string = vowels[new_vowel]
-    elsif consonants.index(string) != nil
-      new_consonant = consonants.index(string) + 1
-      string = consonants[new_consonant]
-    else
-      string = string
-    end
-  end
-end
-loop do
-  puts "Please eneter all names you wish to hide,when finsihed type quit"
-  name = gets.chomp
-  break if name == "quit"
-  alias_array = name.downcase.split(' ')
-  alias_array.reverse!
-
- alias_array.map! do |item|
-    name_split = item.split('')
-    create_alias(name_split)
-  new_name_array = name_split.join('').capitalize
-  end
-  new_name = alias_array.join(" ")
-  modify_hash(alias_hash, name, new_name)
-end
-name_return(alias_hash)
