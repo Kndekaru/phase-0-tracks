79bb2bfb426828b8ff2b161b9655fb82e89f0e3b
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-14 14:53:01 -0400

    add consonant vowel method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,7 +14,8 @@ elsif do same for consonants
 else if no changes are made keep string the same.
 
 =end 
-def name_swapper(user_name)
+
+def name_split(user_name)
   swapped_name = user_name.split(' ').reverse!
   print swapped_name.join(' ')
 end
@@ -22,6 +23,19 @@ puts  "Please enter your full name!"
 user_name = gets.chomp 
 name_swapper(user_name)
 
+def alias_creator(name_split)
+  vowels = "aeiou"
+  consonants = "bcdfghjklmnpqrstvwxy"
 
-
-#consonants = "bcdfghjklmnpqrstvwxy"
+  name_split.map! do |string|
+    if vowels.index(string) != nil
+      new_vowel = vowels.index(string) + 1
+        string = vowels[new_vowel]
+    elsif consonants.index(string) != nil
+      new_consonant = consonants.index(string) + 1
+      string = consonants[new_consonant]
+    else
+      string = string
+    end
+  end
+end
