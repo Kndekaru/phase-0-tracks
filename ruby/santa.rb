0b990e4181d8f00becd05f0804a95d48703ef879
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-16 14:54:04 -0400

    complete release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,5 @@
 class Santa
-attr_reader :age,:ethinicity
+attr_reader :age,:ethnicity
 attr_accessor :gender
 	def speak
 		print "Ho, ho, ho! Haaaappy holidays!"
@@ -28,10 +28,11 @@ attr_accessor :gender
 		@gender =  gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		@age = rand(0..140)
 	end 
 end
-
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid","Transsexual","Androgyne", "N/A"]
+example_ethnicities = ["black", "Latino","Zhuang","Bihari", "white", "Japanese-African", "prefer not to say","Sundanese" ,"Mystical Creature (unicorn)", "N/A"]
 
 #driver code
 =begin
@@ -40,8 +41,6 @@ p test.speak
 p test.eat_milk_and_cookies("sugar cookie")
 miles = Santa.new("agender", "black")
 santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid","Transsexual","Androgyne", "N/A"]
-example_ethnicities = ["black", "Latino","Zhuang","Bihari", "white", "Japanese-African", "prefer not to say","Sundanese" ,"Mystical Creature (unicorn)", "N/A"]
 	example_genders.length.times do |i|
  	santas << Santa.new(example_genders[i], example_ethnicities[i])
 	end
@@ -49,6 +48,7 @@ p santas
 p miles
 
 =end 
+=begin
 #getter an setters
 test =  Santa.new("agender", "black")
 getter = Santa.new("Female","White")
@@ -56,3 +56,15 @@ getter = Santa.new("Female","White")
 #p getter.ethnicity
 test.gender = "cis.male"
 p test.gender
+=end
+#use .sample to gen random element from array
+X = 0
+	until X == 50
+		santa_roster = Santa.new(example_genders.sample,example_ethnicities.sample)
+		puts "Attributes of santa #{X}"
+		puts "Age:#{santa_roster.age}"
+		puts "Gender:#{santa_roster.gender}"
+		puts "Enthnicity:#{santa_roster.ethnicity}"
+		puts "-----------------------------------------"
+		X += 1
+	end
