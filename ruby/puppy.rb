6196dec010482e6b00763674b41a03df4c11337f
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-16 15:07:49 -0400

    outlined puppy class

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,18 +1,24 @@
+=begin
 SPECIES ------------------------
 Canis lupus familiaris
 
 CHARACTERISTICS ----------------
-Size: Large
-Friendly: yes
-Eye count: 2
+Size: Small
+Friendly: false
+Eyecount: 2
 Name: varies
-Color: Brown,white,or black
-Atheletic: Yes
+Color: Brown
+leg_count: 4
+Atheletic: false
+toilet_trained: true
 
 BEHAVIOR -----------------------
 Jump
+play dead
 Bark
 Fetch
 Play
 Beg
 Roll over
+dance
+=end
