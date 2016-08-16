41032993c261f313d483ba6004f16b5920491c80
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-16 12:47:28 -0400

    complete release

---
+++ ruby/santa.rb
@@ -0,0 +1,16 @@
+class Santa
+	def speak
+		print "Ho, ho, ho! Haaaappy holidays!"
+	end 
+	def eat_milk_and_cookies(cookie)
+		print "That was a good #{cookie}"
+	end
+	def initialize
+		print "Initializing Santa instance ..."
+	end 
+end
+
+test =  Santa.new
+
+p test.speak
+p test.eat_milk_and_cookies("sugar cookie")
