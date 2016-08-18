7f0de8b6b4618578ca3ff0e758346741d3f7343d
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-18 12:37:26 -0400

    complete release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -39,3 +39,44 @@ puts puppy.dog_years(12)
 
 puts puppy.stay
 
+class Cars
+  
+  def initialize
+    "New car created"
+  end
+  
+  def start
+    "vroom!"
+  end
+  
+  def drive(speed)
+    puts "Car is going #{speed} mph."
+    speed
+  end
+  
+  def crash
+    "Boom!"
+  end
+  
+end
+
+# car = Cars.new
+# puts car.start 
+# puts car.drive(45)
+# puts car.crash 
+car_lot = []
+
+5.times do
+  car = Cars.new
+  car_lot << car
+end
+
+car_lot.each do |car|
+  puts car.start 
+  puts car.crash 
+    if car.drive(rand(90)) > 60
+      puts "you are going too fast"
+    else
+      puts "you are following th speed limit"
+    end 
+end
