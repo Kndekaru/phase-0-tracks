af889d2992abc5030aab38606fede248c4783694
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-18 13:59:27 -0400

    pseudo code / initial commit

---
+++ ruby/solo.rb
@@ -0,0 +1,19 @@
+=begin
+ 	Declare/Create car class
+	initialize class
+		print that class was initialized
+		create 3 varibales to set atrributes to
+	end method
+
+	delcare travel method take integer as argument
+		add number to milage atrribtes
+		retun new milage
+	end
+	declare repair method takes a string
+		return that string was fixed
+	endmethod
+	declare start method
+	returns car has started
+	end method
+=end	
+
