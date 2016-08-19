5e48b02eb2ef1e77a0115ddbe1c875500b63a268
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-19 11:15:35 -0400

    release 1-2

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -12,27 +12,3 @@ p Shout.yelling_happily("Stop")
 p Shout.yell_angrily("Hello")
 =end
 #release3 mixin
-module Shout
-
-  def yell_angrily(words)
-    puts words + " !*#@ !!!" + " :("
-  end
-
-    def yelling_happily(words)
-    puts words + " YAAAAY! " + " :)"
-  end
-
-end
-class Maniac
-  include Shout
-end
-
-class Kids_at_play
-  include Shout
-end
-
-yell_1= Maniac.new
-yell_1.yell_angrily("Use your turn signal")
-
-yell_2 = Kids_at_play.new
-yell_2.yelling_happily("This is fun")
