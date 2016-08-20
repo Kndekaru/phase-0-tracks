f2171972c02bc853e89730c1c2584019bf37c984
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-20 17:47:11 -0400

    refactor code and have initialize take parameter

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -2,8 +2,10 @@
  	Declare/Create car class
 	initialize class
 		print that class was initialized
-		create 3 varibales to set atrributes to
+		create 4 varibales to set atrributes to
 	end method
+	make method
+		for each folor in array say car comes in that color
 
 	delcare travel method take integer as argument
 		add number to milage atrribtes
@@ -19,12 +21,20 @@
 
 
 class Car
-
-	def initialize
-	"Car has been created"
-	@Make = "Ford"
-	@model = "Focus"
-	@milage = 75000
+	attr_accessor :Make, :Model, :Year, :Milage
+	def initialize(make,model,year,milage)
+	puts "Car has been created"
+	@make = make
+	@year =  year
+	@model = model
+	@milage = milage
+	@colors = ["red","black","blue","green","beige","white"]
+	end
+	def car_colors
+	p "Your #{@Make} comes in these colors below" 
+	@colors.each do |color|
+		puts color
+	end
 	end
 
 	def travel(int)
@@ -32,12 +42,16 @@ class Car
 		updated_milage
 	end
 	def repair(string)
-		"#{string} has been repaired on your #{@Make} #{@Model}"
+		"#{string} has been repaired on your #{@make} #{@model}"
 	end
 
 	def car_start
-		"The car has started succesfuly"
+		p "The car has started succesfuly"
 	end
-
 end
 
+new = Car.new('Ford','Focus',1994,75000)
+p new.car_colors
+p new.travel(4000)
+p new.repair("tailgate")
+p new.car_start
