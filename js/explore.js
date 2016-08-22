e90748430dc7009aef7ab796ee4f0b49dc096afc
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-22 10:53:00 -0400

    create reveser function with for loop

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,12 @@ declare variable to store character from index value
 start for loop
 	for each charatcerin string shift index value in relation to string length
 store shifted values in new variable to be printed as reverse  string
+*/
+function reverse(str) {
+  var index = [];
+  if (str.length > 0){
+  for (var x = str.length - 1, y = 0; x >= 0; x--, y++)
+    index[x] = str[y];
+  return index.join('');
+	}
+}
