ad6679dcac501153cd5ab07cf7bfac82fcd74c89
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-08-26 09:54:04 -0400

    initial commit/gps 6

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,6 +1,6 @@
 # Virus Predictor
 
-# I worked on this challenge [by Miles McArdle-Coe, with:louis Rowan ].
+# I worked on this challenge [by myself, with: ].
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
@@ -9,21 +9,21 @@
 require_relative 'state_data'
 
 class VirusPredictor
-  # When a new instance of virus predictor is created it declares and give values to our instance variables. 
+
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-  # runs thwo methods later defines in the class
+
   def virus_effects
-    predicted_deaths
-    speed_of_spread
+    predicted_deaths(@population_density, @population, @state)
+    speed_of_spread(@population_density, @state)
   end
 
   private
-# this method takes our instance variables as arguments and calculates a new  rounded down value for number of deaths based on  population and states then prints a result.
-  def predicted_deaths
+
+  def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -40,8 +40,8 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-# this method takes 2 arguments and sets a new speed variable  then recalculates the speed varibale based on population density.
-  def speed_of_spread #in months
+
+  def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -69,7 +69,7 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
-=begin
+
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
 
@@ -81,16 +81,7 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
-=end
-STATE_DATA.each do|state_key, state_attributes|
-  state = VirusPredictor.new(state_key, state_attributes[:population_density],state_attributes[:population])
-  state.virus_effects
-end 
+
 
 #=======================================================================
 # Reflection Section
-# the rocket syntax must be used for the states because they are not symbols. The other syntax of key: value can be used for the nested hash because symbols are used as keys
-#require_relative  allows us to pull data/code from another file in some cases another directory and allows the infiormation to be used together.Require allows us to allows us to pull code from built in ruby features rather than indicated files. 
-# few ways to iterate through a hash are using the built it .each and .map methods, the former will loop through each key value pair without modifyinig the hash while the latter is a destructive version 
-#What was odd was that they called instance variables as parameters even though when we had them initialized in a method. having them called through the program was redundant.
-#the concepts i solidified the most was my understanding of require and require_relative and how they differ cn cause the program to behave. i also became more clear  about scope and how private effectsthe data in our class nd how we can navigate around that to make working code.
