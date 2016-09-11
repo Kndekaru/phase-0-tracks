c0a9c56c7e56184c5484b7eb7b3558a995930ec2
Miles H. McArdle-Coe <stealth106@dejazzd.com>
2016-09-11 18:03:49 -0400

    final submit phase 0

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -20,8 +20,31 @@ end
 # create new students via
 # a form
 post '/students' do
+  puts params['name']
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
 
 # add static resources
+#adding in a student lookup page
+
+get '/student_search/new' do
+  erb :student_search
+end
+
+post '/student_search' do
+  name = params['name'].to_s
+  new_url = '/student_search_result/' + params['name'].to_s
+  puts new_url
+ 
+  redirect new_url
+end
+
+#get '/student_search_result/:name' do
+#get '/student_search_result/ do
+get '/student_search_result/' do
+  name = params[:name].to_s
+  @student_result = db.execute("SELECT * FROM students WHERE name=?", [name])
+  #@student_result = db.execute("SELECT * FROM students")
+  erb :student_search_result
+end
